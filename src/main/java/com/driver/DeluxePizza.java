3ee9436fd3a59b4805ba9544f8cd3aaddfac3ac3
Taniya <tanya.kalra200@gmail.com>
2023-02-11 15:40:20 +0530

    First Submission

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,7 +3,10 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
+        //calling constructor of pizza class (parent class)
         super(isVeg);
         // your code goes here
+        addExtraCheese();
+        addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,81 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int toppings;
+    boolean isExtraCheeseAdded;
+    boolean isExtraToppingAdded;
+    boolean isBillCreated;
+    boolean isTakeAway;
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price=300;
+            this.toppings=70;
+        }
+        else{
+            this.price=400;
+            this.toppings=120;
+        }
+        this.isExtraCheeseAdded=false;
+        this.isBillCreated=false;
+        this.isTakeAway=false;
+        this.isExtraToppingAdded=false;
+
+        this.bill = "Base Price Of The Pizza: " + this.price + "\n";
+
     }
 
     public int getPrice(){
+
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        if(isExtraCheeseAdded==false){
+            this.price+=80;
+            isExtraCheeseAdded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isExtraToppingAdded==false){
+            this.price+=this.toppings;
+            isExtraToppingAdded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(isTakeAway==false){
+            this.price+=20;
+            isTakeAway=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(isBillCreated==false)
+        {
+            if (isExtraCheeseAdded) {
+                this.bill += "Extra Cheese Added: 80" + "\n";
+            }
+            if (isExtraToppingAdded) {
+                this.bill += "Extra Toppings Added: " + this.toppings + "\n";
+
+            }
+            if (isTakeAway) {
+                this.bill += "Paperbag Added: 20" + "\n";
+            }
+            this.bill += "Total Price: " + this.price;
+            isBillCreated =true;
+            return this.bill;
+        }
+        return "";
     }
 }
